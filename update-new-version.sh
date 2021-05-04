ce46b94592aea986f54621e5ceb095209bb0dd88
Marcel Kapfer <opensource@mmk2410.org>
2021-05-04 17:57:02 +0000

    Bump Ubuntu version to 21.04 (hirsute)

--- update-new-version.sh
+++ update-new-version.sh
@@ -3,7 +3,7 @@
 # This script intends to decrease the effort of updating the package.
 
 PACKAGE="intellij-idea-community"
-DISTRIBUTION="groovy"
+DISTRIBUTION="hirsute"
 
 main() {
     last_tag=$(git describe --abbrev=0 --tags)
